ca820db2be7430e9790518e01a57a56c5324cb79
Damian Senn <senn@puzzle.ch>
2016-10-24 12:42:06 +0200

    Add minor comment about device readiness

--- app/initializers/deviceready.js
+++ app/initializers/deviceready.js
@@ -1,3 +1,6 @@
+// This is fine for now, once we use loading templates, we should
+// handle this in our application route.
+// http://hussfelt.net/2015/08/06/how-to-stop-using-deferreadiness-and-advancereadiness-in-ember/
 export function initialize(application) {
   if (window.cordova) {
     application.deferReadiness()
