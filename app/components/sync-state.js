031f4b3f294ef6eb11f66aa123454cd78a1f6b27
Damian Senn <damian.senn@topaxi.codes>
2016-10-27 15:08:40 +0200

    Sync state styling

--- app/components/sync-state.js
+++ app/components/sync-state.js
@@ -1,9 +1,23 @@
+/* eslint-disable no-magic-numbers */
 import Ember from 'ember'
 
-const { Component, inject } = Ember
+const {
+  Component,
+  inject,
+  computed,
+  String: { htmlSafe }
+} = Ember
 
 export default Component.extend({
   tagName: '',
 
-  sync: inject.service()
+  sync: inject.service(),
+
+  progressStyle: computed('sync.syncProcessed', 'sync.syncTotal', function() {
+    let total = this.get('sync.syncTotal')
+    let processed = this.get('sync.syncProcessed')
+    let percent = 100 / total * processed
+
+    return htmlSafe(`--progress:${(percent || 0).toFixed(2)}%`)
+  })
 })
